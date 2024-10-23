e3ef654284aa8bfd8dab52d9f34a9a5bffd753ef
criticbanggu@gmail.com <criticbanggu@gmail.com>
2024-10-23 18:22:38 +0900

    for문 사용하기

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <!-- 실제로 html 로 렌더링 되는 곳 -->
+  <div>
     <h1>영화정보</h1>
     <div>
       <!-- :속성명 = "데이터" -->
@@ -7,6 +8,13 @@
       <p>개봉 : {{ year }}</p>
       <p>장르 : {{ category }}</p>
     </div>
+    <p>{{ foods[0] }}</p>
+    <p>{{ foods[1] }}</p>
+    <p>{{ foods[2] }}</p>
+    <!-- for문 사용하기 "v-for="(아이템, key)in 배열데이터"" -->
+    <!-- key 속성 없으면 에러뜸 -->
+    <p v-for="(item, i) in foods" :key="i">{{ item }}</p>
+  </div>
 </template>
 
 <script>
@@ -15,6 +23,7 @@ export default {
   //문서에 표시될 변수를 선언할 수 있음 (state)
   data() {
     return {
+      foods: ["김밥", "순대", "만두"],
       title: "노량",
       year: 2023,
       category: "액션, 드라마",
@@ -24,7 +33,6 @@ export default {
 };
 </script>
 
-<style></style>
 <style>
 .bg-yellow {
   background-color: gold;
